6a6326578e3d8972eacc761e06cf7b737bd1d8b2
mohamed saad <mohamed.saad@shelter-technology.com>
2024-09-17 02:12:55 +0300

    add models

--- stock_system/base/models.py
+++ stock_system/base/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from .utils import update_stock
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    stock = models.DecimalField(max_digits=10, decimal_places=2)
+    low_stock_alert_sent = models.BooleanField(default=False)
+    initial_stock = models.DecimalField(max_digits=10, decimal_places=2, default=1000)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    ingredients = models.ManyToManyField(Ingredient, through="ProductIngredient")
+
+
+class ProductIngredient(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class OrderData(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    order = models.ForeignKey(
+        "Order", on_delete=models.CASCADE, related_name="order_data"
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        update_stock(self)
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    ### add more fields as needed
